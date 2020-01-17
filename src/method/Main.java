58bdb0463274b271efa07c591b5753dd4eb6a30c
Lazuta <lenya.lazuta@mail.ru>
2020-01-17 19:34:18 +0300

    Modification lab14

--- src/method/Main.java
+++ src/method/Main.java
@@ -5,11 +5,11 @@ public class Main {
         String key = "Key";
         String message = "Email";
 
-        Vigenere vigenere =  new Vigenere(message, key);
+        Vigenere vigenere =  new Vigenere(message, key, true);
 
         String encryptedMsg = vigenere.encrypt();
 
-        System.out.println("Строка: " + message);
+        System.out.println("Введенная строка: " + message);
         System.out.println("Закодированная строка: " + encryptedMsg);
         System.out.println("Декодированная строка: " + vigenere.decrypt(encryptedMsg));
 
--- src/method/Vigenere.java
+++ src/method/Vigenere.java
@@ -3,40 +3,54 @@ package method;
 class Vigenere {
     protected String word;
     protected String key;
+    protected boolean mode;
 
-    Vigenere(String word, String key) {
+    Vigenere(String word, String key, boolean mode) {
         this.word = word;
         this.key = key;
+        this.mode = mode;
     }
 
-    public String encrypt()
-    {
-        String res = "";
-        this.word = this.word.toUpperCase();
-        for (int i = 0, j = 0; i < this.word.length(); i++)
-        {
-            char c = this.word.charAt(i);
-            if (c < 'A' || c > 'Z')
-                continue;
-            res += (char) ((c + this.key.charAt(j) - 2 * 'A') % 26 + 'A');
-            j = ++j % this.key.length();
+    public String encrypt() {
+        StringBuilder res = new StringBuilder();
+
+        try {
+            this.word = this.word.toUpperCase();
+            for (int i = 0, j = 0; i < this.word.length(); i++) {
+                char c = this.word.charAt(i);
+                if (c < 'A' || c > 'Z')
+                    continue;
+                res.append((char) ((c + this.key.charAt(j) - 2 * 'A') % 26 + 'A'));
+                j = ++j % this.key.length();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (mode) System.out.println("\u001B[34m" + "Encrypt bloсk finally" + "\u001B[0m");
         }
-        return res;
+
+        return res.toString();
     }
 
-    public String decrypt(String text)
-    {
-        String res = "";
-        text = text.toUpperCase();
-        for (int i = 0, j = 0; i < text.length(); i++)
-        {
-            char c = text.charAt(i);
-            if (c < 'A' || c > 'Z')
-                continue;
-            res += (char) ((c - this.key.charAt(j) + 26) % 26 + 'A');
-            j = ++j % this.key.length();
+    public String decrypt(String text) {
+        StringBuilder res = new StringBuilder();
+
+        try {
+            text = text.toUpperCase();
+            for (int i = 0, j = 0; i < text.length(); i++) {
+                char c = text.charAt(i);
+                if (c < 'A' || c > 'Z')
+                    continue;
+                res.append((char) ((c - this.key.charAt(j) + 26) % 26 + 'A'));
+                j = ++j % this.key.length();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (mode) System.out.println("\u001B[34m" + "Decrypt bloсk finally" + "\u001B[0m");
         }
-        return res;
+
+        return res.toString();
     }
 
 }
